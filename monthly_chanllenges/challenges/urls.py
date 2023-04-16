91ac14bd62f47044da7a2fd5c0d92aee7da27143
Jeffrey Mcintyre <redeyecoding@gmail.com>
2023-04-16 10:56:32 -0400

    2nd commit

--- monthly_chanllenges/challenges/urls.py
+++ monthly_chanllenges/challenges/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # if a request comes into challenges/january then execute the jeff_index function.
 urlpatterns = [
-    path("<jeff_month>", views.monthly_challenge)
+    path("<int:jeff_month_by_number>", views.monthly_challenge_by_numbers),
+    path("<str:jeff_month>", views.monthly_challenge)
 ]
--- monthly_chanllenges/challenges/views.py
+++ monthly_chanllenges/challenges/views.py
@@ -19,6 +19,25 @@ from django.http import HttpResponse, HttpResponseNotFound
 # def may(request):
 #     return HttpResponse("Its May..may showers")
 
+def monthly_challenge_by_numbers(request, jeff_month_by_number):
+    match jeff_month_by_number:
+        case 1:
+            return HttpResponse("Its january but in number format.")
+        case 2:
+            return HttpResponse("Its february but in number format.")
+        case 3:
+            return HttpResponse("Its march but in number format.")
+        case 4:
+            return HttpResponse("Its april but in number format.")
+        case 5:
+            return HttpResponse("Its may but in number format.")
+        case 6:
+            return HttpResponse("Its june but in number format.")
+        case _:
+            return HttpResponseNotFound("This Month is Not Supported")
+        
+    return HttpResponseNotFound("nope")
+
 def monthly_challenge(request, jeff_month):
     match jeff_month:
         case 'january':
